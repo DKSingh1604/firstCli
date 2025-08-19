7e6fe4a12139702614a9a05f7f0cbf10e92cd22f
Dev Karan Singh <dev1604karan@gmail.com>
2025-08-20 00:05:31 +0530

    Refactor App component and add CompanyData component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
-import { View, Text } from 'react-native';
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import CompanyData from './components/companyData';
+import type { FC } from 'react';
 
-function App() {
+const App: FC = () => {
+  let data = 10;
+
+  const fruit = (val: any) => {
+    console.warn(val);
+  };
   return (
     <View>
-      <Text style={{ fontSize: 50 }}>HEHE BOI</Text>
+      <CompanyData />
+      <Text style={{ fontSize: 30 }}>{data}</Text>
+      <Button
+        title="On Press"
+        color={'red'}
+        onPress={() => fruit('Hello DK')}
+      />
+      <Button title="On Press 2" color={'green'} onPress={() => fruit('Lio')} />
     </View>
   );
-}
+};
 
 export default App;
---
+++ components/companyData.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+
+const CompanyData = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>Name: ABC Company</Text>
+      <Text style={{ fontSize: 30 }}>Total Emp: 500</Text>
+      <Text style={{ fontSize: 30 }}>Product: anything</Text>
+    </View>
+  );
+};
+export default CompanyData;
